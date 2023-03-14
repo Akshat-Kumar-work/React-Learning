aba95fdc1fcac700d77d196eda1d658981ed4ff1
Akshat <iamakku0.0.0.1@gmail.com>
2023-03-14 14:45:58 +0530

    commiting on 14 march 23

--- src/App.js
+++ src/App.js
@@ -2,11 +2,50 @@ import './App.css';
 import List from './components/item'; //importing List component from a item file present in component folder
 
 function App() {
-  return (
+
+  //ek array k andar object bna rkhe hai usme listname aur listdate ki values hai
+  const response = [
+    {
+      listname:"grocery list",
+      listdate:"14 march 23"
+    },
+    {
+      listname:"shopping list",
+      listdate:"23 march 23"
+    }
+
+  ]
+
+  return ( 
     //hamesha return karty hue ek single element k andar hona chaie sara code
     <div>
+
   
-    <List></List>
+
+ {/* list component m jo naam property hai usme response naam  k array m jo 0th aur 1th index par object hai usko daldo  */}
+  <List 
+   name={response[0].listname}
+   date={response[0].listdate} >
+   {/* , jo hum content dalty hai component k andar vo by default visible nahi hota 
+   usko visible krane k lie hume {props.children} ko return krwana pdega component m */}
+     item first 
+   </List>
+
+
+  <List 
+  name={response[1].listname}
+   date={response[1].listdate} >
+   </List>
+
+
+
+
+   
+    <List name="avnish"></List>  {/* yaha humne List component ki properties ko pass kiya  hai  */}
+    <List name="rohan"></List>
+    <List name="wil"></List>
+
+    <List date="1 march 23"></List>
 <div className="App">
        hello ji
       </div>
--- src/components/item.js
+++ src/components/item.js
@@ -2,9 +2,25 @@ import './item.css';
 
 
 //List component
-function List (){
+function List (props){ //props parameter hai List ki jo app.js m declare hue hai
 
-    return(<p className="colour"> orange</p>);
+    // naam ek propety hai list component ki
+    const naam = props.name; //jo argument pass hue hai app.js m unko access kar rhe hai 
+    const date = props.date;
+    
+    return(
+        <div>
+
+        {naam} {/*using variable values*/ } 
+        {date}
+        
+        {/* by this we can access the content of component */}
+        {props.children} 
+        {/* <p className="colour"> orange</p> */}
+       
+        </div>
+    
+    );
 
 }
 
