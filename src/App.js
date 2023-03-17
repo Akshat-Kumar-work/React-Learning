dbf3e461910431bb286c0369a4b788d6ad415df8
Akshat <iamakku0.0.0.1@gmail.com>
2023-03-17 14:47:25 +0530

    commiting on 17 march 23

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
- import ItemDate from './components/ItemDate.js';
- import Item from './components/Item.js';
-import Card from './components/Card.js';
+//  import ItemDate from './components/ItemDate.js';
+//  import Item from './components/Item.js';
+// import Card from './components/Card.js';
+import Products from "./components/Products";
+
 function App() {
 
-  const response = [
+  // const response = [
    
-    {
-      itemname:"nirma",
-      Date:"20",
-      month :"june",
-      year:"1998"
+  //   {
+  //     itemname:"nirma",
+  //     Date:"20",
+  //     month :"june",
+  //     year:"1998"
 
-    },
+  //   },
 
-    {
-      itemname:"tide",
-      Date:"20",
-      month :"june",
-      year:"1998"
+  //   {
+  //     itemname:"tide",
+  //     Date:"20",
+  //     month :"june",
+  //     year:"1998"
 
-    },
+  //   },
 
-    {
-      itemname:"surf excel ",
-      Date:"20",
-      month :"june",
-      year:"1998"
+  //   {
+  //     itemname:"surf excel ",
+  //     Date:"20",
+  //     month :"june",
+  //     year:"1998"
 
-    },
+  //   },
+  // ]
 
 
+  //product variable bna rkha hai 
+  const products = [
+  {
+    id:'p1',
+    title:'Nirma',
+    amount:100,
+    date:new Date(2021,8,10),
+  },
+  {
+    id:'p2',
+    title:'Surf excel',
+    amount:102,
+    date:new Date(2021,8,10),
+  },
+  {
+    id:'p3',
+    title:'Tide',
+    amount:103,
+    date:new Date(2021,8,10),
+  },
+  {
+    id:'p4',
+    title:'ariel',
+    amount:104,
+    date:new Date(2021,8,10),
+  },
+  ]; 
 
-  ]
+
   return ( 
     
     //hamesha return karty hue ek single element k andar hona chaie sara code
     <div>
 
 
-    <Card>
+    {/* <Card>
 
-    <Item name={response[0].itemname} > item name ka children hu m</Item>
+    <Item name={response[0].itemname} >  </Item>
     <ItemDate day={response[0].day} month={response[0].month} year={response[0].year}></ItemDate>
 
     <Item name={response[1].itemname}></Item>
@@ -48,11 +78,13 @@ function App() {
 
     <Item name={response[2].itemname}></Item>
     <ItemDate day={response[2].day} month={response[2].month} year={response[2].year}></ItemDate>
+     
+    </Card> */}
     
-
-    </Card>
     
+    {/* Products components k lie items prop m products pass kardiye */}
     
+    <Products items={products}></Products>
 
     </div>
     
---
+++ src/components/Productitem.js
@@ -0,0 +1,23 @@
+import Card from './Card.js';
+import ProductDate from './Productdate.js';
+import './Productitem.css';
+const Productitem = (props)=>{
+const amount = props.amount;
+    return(
+        
+        <Card className='product-item'>
+
+        <ProductDate date={props.date}/>
+
+        <div className='product-item-decription'>
+            <h2>{props.title}</h2>
+        </div>
+
+        <div>{amount}</div>
+           
+        </Card>
+        
+    );
+}
+
+export default Productitem;
