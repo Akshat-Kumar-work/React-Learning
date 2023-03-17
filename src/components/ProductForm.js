4451d2b4c6c22848db8e5d3cee2426502d334ccf
Akshat <iamakku0.0.0.1@gmail.com>
2023-03-18 00:23:43 +0530

    commiting on 18 march 23

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -5,33 +5,45 @@ import {useState} from 'react';
 
 function ProductForm(){
 
-   const [title,setTitle] = useState('');
-   const [date,setDate] = useState('');
+   const [newtitle,setTitle] = useState('');
+   const [newdate,setDate] = useState('');
 
     // this event  Object contain many info about the event occuring 
     function titleChangeHandler(event){
        // console.log(event.target.value)
-        setTitle(event.target.value)
+       setTitle(event.target.value)  //jispe event lga hua hai uss element ki value ko pass kar rhe hai
     }
 
     function dateChangeHandler(event){
        // console.log(event.target.value);
-        setDate(event.target.value)
+         setDate(event.target.value)
+    }
 
-      
+    function submitHandler(event){
+        event.preventDefault();
+
+        const productData = {
+            title:newtitle,
+            date:newdate,
+        };
+
+        console.log(productData)
+
+        setTitle('')
+        setDate('')
     }
     return(
-       <form>
+       <form onSubmit={submitHandler}>
 
         
         <div className="new-product-title">
         <label>Title</label>
-        <input type='text' onChange={titleChangeHandler}></input>
+        <input type='text' value={newtitle} onChange={titleChangeHandler}></input>
         </div>
       
         <div className="new-product-date">
         <label>Date</label>
-        <input type='date' onChange={dateChangeHandler} min='2023-01-01' max='2023-12-12'></input>
+        <input type='date' value={newdate} onChange={dateChangeHandler} min='2023-01-01' max='2023-12-12'></input>
         </div>
         
         <div className="new-product-button">
